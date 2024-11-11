84635dde4901b6706d14493f13c4a8d29d0535a9
rafmasloman <raflymasloman20@gmail.com>
2024-11-11 12:31:08 +0800

    feat : initialize project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="bg-neutral-900 py-4 px-8 text-white">
+    <ul class="flex">
+      <li>
+        <RouterLink to="/">Homepage</RouterLink>
+      </li>
+    </ul>
+  </nav>
+
+  <RouterView />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
